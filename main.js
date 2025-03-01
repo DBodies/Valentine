27d8907ef7fe2e87ef6f43b7f093175f2b683098
Denis <159953887+DBodies@users.noreply.github.com>
2025-03-01 15:46:17 +0200

    JSpre-Final

--- main.js
+++ main.js
@@ -47,6 +47,35 @@ document.addEventListener("DOMContentLoaded", function () {
                 document.getElementById("contactMe").textContent = i18next.t("contactMe");
                 document.getElementById("textUnderName").textContent = i18next.t("textUnderName");
                 document.getElementById("openToWork").textContent = i18next.t("openToWork");
+        document.getElementById("secondTitle").textContent = i18next.t("secondTitle");
+        document.getElementById("myName").textContent = i18next.t("myName");
+        document.getElementById("secondText").textContent = i18next.t("secondText");
+        document.getElementById("educationXAI").textContent = i18next.t("educationXAI");
+        document.getElementById("speciality").textContent = i18next.t("speciality");
+        document.getElementById("goIT").innerHTML = i18next.t("goIT");
+        document.getElementById("lngUaEn").innerHTML = i18next.t("lngUaEn");
+        document.getElementById("design").innerHTML = i18next.t("design");
+                document.getElementById("btnReadMore").textContent = i18next.t("btnReadMore");
+                document.getElementById("btnReadMore").textContent = i18next.t("btnReadMore");
+                document.getElementById("secondDscr").textContent = i18next.t("secondDscr");
+                document.getElementById("projectDscr").textContent = i18next.t("projectDscr");
+                document.getElementById("thirdDscr").textContent = i18next.t("thirdDscr");
+                document.getElementById("projectTitle").textContent = i18next.t("projectTitle");
+                document.getElementById("additionalSkills").textContent = i18next.t("additionalSkills");
+                document.getElementById("sphereSkills").textContent = i18next.t("sphereSkills");
+                document.getElementById("mySkillsTitle").textContent = i18next.t("mySkillsTitle");
+                document.getElementById("toolsSkills").textContent = i18next.t("toolsSkills");
+                document.getElementById("knowledgeSkills").textContent = i18next.t("knowledgeSkills");
+        document.getElementById("adaptation").textContent = i18next.t("adaptation");
+                document.getElementById("animation").textContent = i18next.t("animation");
+                document.getElementById("layot").textContent = i18next.t("layot");
+                document.getElementById("interface").textContent = i18next.t("interface");
+                document.getElementById("experience").textContent = i18next.t("experience");
+                document.getElementById("mobileDesign").textContent = i18next.t("mobileDesign");
+                document.getElementById("webDesign").textContent = i18next.t("webDesign");
+                document.getElementById("designSystem").textContent = i18next.t("designSystem");
+                document.getElementById("projectTitle").textContent = i18next.t("projectTitle");
+                document.getElementById("languageSpan").textContent = i18next.t("languageSpan");
 
         // Обновляем текст анимации
         text = i18next.t("animatedText");
@@ -75,12 +104,40 @@ document.addEventListener("DOMContentLoaded", function () {
                 "textUnderName": "I'm a junior UI/UX Designer. I can and know how to create modern website designs mobile apps, know how to customize them",
                 "contactMe": "Contact me",
                 "openToWork": "Open to work",
+                    "secondTitle": "About me",
+                    "myName": "I`m Valentyn",
+                    "secondText": "I specialize in UI/UX design for web and mobile applications. As I continue to grow in this field, I strive to bring ideas to life by creating solutions that are not only aesthetically pleasing but also highly functional, simplifying users' everyday tasks.",
+                    "educationXAI": "I have a higher education in the field of IT, I graduated from National Aerospace University - Kharkiv Aviation Institute",
+                    "speciality": "121 Specialty 121 Software Engineering",
+                    "goIT": "Completed UI/UX Designer courses at GoIt <br /> February 2024 - September 2024",
+                    "btnReadMore": "Read more",
+                "projectDscr": "An app for finding and attending events",
+                "secondDscr": "Foreign language learning app",
+                "thirdDscr": "A site showing the capabilities of designers",
+                    
+                    "projectTitle": "My projects",
+                    "designSystem": "Design system",
+                    "webDesign": "Web design",
+                    "mobileDesign": "Mobile design",
+                    "experience": "User experience",
+                    "interface": "User interface",
+                    "layot": "Layout",
+                    "animation": "Animation",
+                    "adaptation": "Adaptation",
+                    "knowledgeSkills": "Knowledge",
+                    "toolsSkills": "Tools",
+                    "additionalSkills": "Additional knowledge",
+                    "design": "UI/UX design<br />Web design",
+                    "sphereSkills": "Sphere",
+                    "lngUaEn": "Ukrainian<br />English",
+                    "mySkillsTitle": "Languages",
+                    "languageSpan": "Languages"
                     
                 },
             },
             ua: {
                 translation: {
-                    "animatedText": "Гарний дизайн - це не тільки форма, а й ставлення. Він повинен вирішувати проблеми, а не створювати їх.",
+                    "animatedText":"Гарний дизайн - це про ставлення. Він повинен вирішувати проблеми, а не створювати їх.",
                     "firstName": "Валентин",
                     "myHome": "Головна",
                     "aboutMe": "Про мене",
@@ -89,14 +146,43 @@ document.addEventListener("DOMContentLoaded", function () {
                     "myContact": "Контакти",
                     "toggleButton": "EN",
                     "btnContacbtn": "Контакти",
-                     "neelov": "NEELOV",
+                     "neelov": "Неєлов",
                 "valentyn": "VALENTYN",
                 "textUnderName": "Я junior UI/UX Designer. Я вмію та знаю, як створювати сучасні дизайни вебсайтів і мобільних додатків, а також як їх налаштовувати",
                 "contactMe": "Зв'язатися зі мною",
-                "openToWork": "Готовий до співпраці",
+                    "openToWork": "Готовий до співпраці",
+                    "secondTitle": "Про мене",
+                    "myName": "Я Валентин",
+                    "secondText": "Я спеціалізуюся на UI/UX дизайні для веб- та мобільних додатків. Продовжуючи розвиватися в цій сфері, я втілюю ідеї в життя, створюючи рішення, які є естетично привабливими та максимально функціональними, спрощуючи повсякденні завдання користувачів.",
+                    "educationXAI": "Маю вищу освіту в сфері ІТ. Закінчив Національний аерокосмічний університет.",
+                    "speciality": "Спеціальність 121 Інженерія програмного забезпечення",
+                    "goIT": "Пройшов курс UI/UX дизайну в GoIt <br /> (лютий 2024 — вересень 2024).",     
+                "btnReadMore": "Про курс",
+                "projectDscr": "Додаток для пошуку та відвідування заходів",
+                "secondDscr": "Додаток для вивчення іноземних мов",
+                "thirdDscr": "Сайт, що демонструє можливості дизайнерів",
+                    "projectTitle": "Мої проєкти",
+                 "designSystem": "Дизайн системи",
+                 "webDesign": "Вебдизайн",
+                 "mobileDesign": "Мобільний дизайн",
+                 "experience": "Досвід користувача",
+                    "interface": "Інтерфейс користувача",
+                    "layot": "Верстка",
+                    "animation": "Анімація",
+                    "adaptation": "Адаптація",
+                    "knowledgeSkills": "Знання",
+                    "toolsSkills": "Інструменти",
+                    "additionalSkills": "Додаткові знання",
+                    "design": "UI/UX дизайн <br /> Вебдизайн",
+                    "sphereSkills": "Отрасль",
+                    "lngUaEn": "Українська <br />Англійська",
+                    "mySkillsTitle": "Мої навички",
+                   "languageSpan": "Мови"
+                 
                 },
             },
         },
+        interpolation: { escapeValue: false }
     }, function () {
         updateTexts();
     });
